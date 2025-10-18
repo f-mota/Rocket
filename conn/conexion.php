753afe3b62cd8dd3fcff946b1ffb0da8a5de0cba
Nicolás Servidio <nicolasservidio.dm@gmail.com>
2025-10-18 01:55:24 -0300

    Refactor ConexionBD to use environment variables in production
    Updated the ConexionBD function to automatically select database credentials based on the HTTP_HOST. In production, credentials are now read from environment variables, improving security and flexibility. The old function signature is commented out for reference.

--- conn/conexion.php
+++ conn/conexion.php
@@ -1,27 +1,33 @@
 <?php
 
-if ($_SERVER['HTTP_HOST'] === 'localhost') {
-
-    function ConexionBD($Host = "localhost", $User = "root", $Password = "", $BaseDeDatos = "rocket") {
-        
-        $linkConexion = mysqli_connect($Host, $User, $Password, $BaseDeDatos);
-        mysqli_set_charset($linkConexion, "utf8mb4");
-        if ($linkConexion != false) {
-            return $linkConexion;
-        } 
-        else {
-            die("No se pudo establecer la conexión.");
-        }
+function ConexionBD() {
+    if ($_SERVER['HTTP_HOST'] === 'localhost') {
+        $host = "localhost";
+        $user = "root";
+        $pass = "";
+        $db   = "rocket";
+    } else {
+        $host = getenv('MYSQLHOST');
+        $user = getenv('MYSQLUSER');
+        $pass = getenv('MYSQLPASSWORD');
+        $db   = getenv('MYSQLDATABASE');
     }
-} 
-
-else {
-    $host = getenv('MYSQLHOST');
-    $user = getenv('MYSQLUSER');
-    $pass = getenv('MYSQLPASSWORD');
-    $db   = getenv('MYSQLDATABASE');
 
     $linkConexion = mysqli_connect($host, $user, $pass, $db);
+    mysqli_set_charset($linkConexion, "utf8mb4");
+
+    if ($linkConexion != false) {
+        return $linkConexion;
+    } else {
+        die("No se pudo establecer la conexión.");
+    }
+}
+
+/*
+function ConexionBD($Host = "localhost", $User = "root", $Password = "", $BaseDeDatos = "rocket") {
+    
+    $linkConexion = mysqli_connect($Host, $User, $Password, $BaseDeDatos,);
+    mysqli_set_charset($linkConexion, "utf8mb4");
     if ($linkConexion != false) {
         return $linkConexion;
     } 
@@ -29,5 +35,7 @@ else {
         die("No se pudo establecer la conexión.");
     }
 }
+
+*/
 
 ?>

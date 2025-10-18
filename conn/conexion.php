50d66a5b72f8ac9d426a8294ba7c1e00ae11957a
Nicolás Servidio <nicolasservidio.dm@gmail.com>
2025-10-18 00:13:45 -0300

    Add Adminer tool and update database connection
    Added the Adminer PHP file (version 5.4.1) for database management. Updated 'conn/conexion.php' to support integration or configuration changes related to Adminer with the Railway platform for deployment and hosting.

--- conn/conexion.php
+++ conn/conexion.php
@@ -1,9 +1,27 @@
 <?php
 
-function ConexionBD($Host = "localhost", $User = "root", $Password = "", $BaseDeDatos = "rocket") {
-    
-    $linkConexion = mysqli_connect($Host, $User, $Password, $BaseDeDatos,);
-    mysqli_set_charset($linkConexion, "utf8mb4");
+if ($_SERVER['HTTP_HOST'] === 'localhost') {
+
+    function ConexionBD($Host = "localhost", $User = "root", $Password = "", $BaseDeDatos = "rocket") {
+        
+        $linkConexion = mysqli_connect($Host, $User, $Password, $BaseDeDatos);
+        mysqli_set_charset($linkConexion, "utf8mb4");
+        if ($linkConexion != false) {
+            return $linkConexion;
+        } 
+        else {
+            die("No se pudo establecer la conexión.");
+        }
+    }
+} 
+
+else {
+    $host = getenv('MYSQLHOST');
+    $user = getenv('MYSQLUSER');
+    $pass = getenv('MYSQLPASSWORD');
+    $db   = getenv('MYSQLDATABASE');
+
+    $linkConexion = mysqli_connect($host, $user, $pass, $db);
     if ($linkConexion != false) {
         return $linkConexion;
     } 
